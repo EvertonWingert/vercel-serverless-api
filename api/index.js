318f5017057b920d614dec926a61f94e57ef4810
Everton <54149497+EvertonWingert@users.noreply.github.com>
2022-06-08 16:53:57 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -2,5 +2,5 @@ export default function handler(req, res) {
 	res.statusCode = 200;
 	res.setHeader("Content-Type", "application/json");
 	res.setHeader("Cache-Control", "s-maxage=1, stale-while-revalidate=59");
-	res.json({ name: "John Doe", date: new Date(), request: req });
+	res.json({ name: "John Doe", date: new Date() });
 }
