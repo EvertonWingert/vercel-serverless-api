574c17b8d6ce83a0b0198dcc7607541ca3afd0b3
Everton <54149497+EvertonWingert@users.noreply.github.com>
2022-06-09 08:24:20 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,6 +1,4 @@
 export default function handler(req, res) {
-	const { name } = req.body;
-
 	res.setHeader("Content-Type", "application/json");
-	res.json({ name: name });
+	res.json({ name: req.body });
 }
